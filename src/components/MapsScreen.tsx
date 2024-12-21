c00789024c185e69ab4a2f8656637a453be392d6
Alice Santoro s324739 <s324739@studenti.polito.it>
2024-12-21 15:23:13 +0100

    Revert "Merge branch 'mapScreen' of github.com:polito-hci-2024/eating-the-world into profile"
    This reverts commit d76fad8ed27227f96ffb35b0291cde74aabc439a, reversing
    changes made to dcd8acd70e44c1d3ead38444aae56840b69ce9ea.
    
    messaggio

---
+++ src/components/MapsScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { styles } from '../styles/styles';
+
+const MapsScreen = () => (
+    <View style={styles.container}>
+      <Text style={styles.text}>Maps Screen</Text>
+    </View>
+);
+
+export { MapsScreen }
