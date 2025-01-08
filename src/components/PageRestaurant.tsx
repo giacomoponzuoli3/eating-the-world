d9ec43f02f0ac88d48cc91554c74e689006d9cff
ponzuoligiacomo@gmail.com <ponzuoligiacomo@gmail.com>
2025-01-08 15:52:51 +0100

    introduce styles

--- src/components/PageRestaurant.tsx
+++ src/components/PageRestaurant.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { Restaurant } from "../utils/interfaces";
 import { View, TouchableOpacity, Text, ImageBackground } from "react-native";
 import { Icon } from "react-native-elements";
@@ -14,37 +14,48 @@ interface PageRestaurantProps{
 }
 
 const PageRestaurant: FC<PageRestaurantProps> = ({ restaurant, onClose, user}: any) => {
-    return (
-      <View style={stylesPageRestaurant.container}>
-        <View>
-          <ImageBackground 
-            source={imagesRestaurants[restaurant.name]} 
-            style={stylesPageRestaurant.imageBackground}
-          > 
-            <View style={stylesPageRestaurant.imagesStyle}>
-              <TouchableOpacity onPress={onClose} style={stylesPageRestaurant.iconWrapper}>
-                <Ionicons name="chevron-back-sharp" size={30} color="white" />
-              </TouchableOpacity>
+  
+  const [isFavorite, setIsFavorite] = useState<Boolean>(false);
+  
+  const modifyFavorite = async () => {
+    if(isFavorite){ //true
+      setIsFavorite(false);
+    }else{
+      setIsFavorite(true);
+    }
+  }
 
-              <TouchableOpacity onPress={onClose} style={stylesPageRestaurant.iconWrapper}>
-                <Ionicons name="star-outline" size={25} color="white" />
-              </TouchableOpacity>
-            </View>
-             
-          </ImageBackground>
-        </View>
+  return (
+    <View style={stylesPageRestaurant.container}>
+      <View>
+        <ImageBackground 
+          source={imagesRestaurants[restaurant.name]} 
+          style={stylesPageRestaurant.imageBackground}
+        > 
+          <View style={stylesPageRestaurant.imagesStyle}>
+            <TouchableOpacity onPress={onClose} style={stylesPageRestaurant.iconWrapper}>
+              <Ionicons name="chevron-back-sharp" size={30} color="white" />
+            </TouchableOpacity>
 
-        <Text style={{ fontSize: 24 }}>{restaurant.name}</Text>
-        <Text style={{ fontSize: 16, marginVertical: 10 }}>{restaurant.description}</Text>
-        <Text>{user.name}</Text>
-
-        <FontAwesome5Icon name="money" size={20}/> <Text>{restaurant.price_range}</Text>
-
-        <View style={{position: 'absolute', left: 0, right: 0, bottom: 0}}>
-          <Text>My fixed footer</Text>
-        </View>
+            <TouchableOpacity onPress={modifyFavorite} style={stylesPageRestaurant.iconWrapper}>
+              <Ionicons name={isFavorite ? "star" : "star-outline"} size={25} color="white" />
+            </TouchableOpacity>
+          </View>
+            
+        </ImageBackground>
       </View>
-    );
+
+      <Text style={{ fontSize: 24 }}>{restaurant.name}</Text>
+      <Text style={{ fontSize: 16, marginVertical: 10 }}>{restaurant.description}</Text>
+      <Text>{user.name}</Text>
+
+      <FontAwesome5Icon name="money" size={20}/> <Text>{restaurant.price_range}</Text>
+
+      <View style={{position: 'absolute', left: 0, right: 0, bottom: 0}}>
+        <Text>My fixed footer</Text>
+      </View>
+    </View>
+  );
 };
 
 
