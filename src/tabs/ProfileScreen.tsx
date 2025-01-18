373e10d6657715c6c048d672b3c8c5fff496a2fa
Alice Santoro s324739 <s324739@studenti.polito.it>
2025-01-18 20:50:26 +0100

    IOS action sheet to change user

--- src/tabs/ProfileScreen.tsx
+++ src/tabs/ProfileScreen.tsx
@@ -4,8 +4,7 @@ import {
   Text,
   Image,
   TouchableOpacity,
-  ScrollView,
-  Modal,
+  ActionSheetIOS,
 } from "react-native";
 import { styles } from "../styles/styles";
 import { Feather, Ionicons } from "@expo/vector-icons";
@@ -13,10 +12,7 @@ import { colors } from "../constants/color";
 import { iconSize } from "../constants/dimensions";
 import { TextInput } from "react-native-gesture-handler";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { Picker } from "@react-native-picker/picker";
 import { User } from "../../App";
-import { updateUser } from "../dao/usersDAO";
-import QRCode from "react-native-qrcode-svg";
 
 type CustomInputProps = {
   label?: string;
@@ -36,8 +32,8 @@ const CustomInput: React.FC<CustomInputProps> = ({
   return (
     <View style={styles.container2}>
       <View style={styles.inputFieldsContainer}>
-      {label && <Text style={styles.label2}>{label}</Text>}
-      {icon && <View style={styles.icon}>{icon}</View>}
+        {label && <Text style={styles.label2}>{label}</Text>}
+        {icon && <View style={styles.icon}>{icon}</View>}
         <TextInput
           style={styles.textInput}
           placeholder={placeholder}
@@ -50,78 +46,17 @@ const CustomInput: React.FC<CustomInputProps> = ({
   );
 };
 
-const Dropdown = ({ users, selectedValue, onValueChange }: any) => {
-  return (
-    <View style={styles.container3}>
-      <Text style={styles.modalTitle}> Select a user:</Text>
-      <Picker
-        selectedValue={selectedValue}
-        onValueChange={onValueChange}
-        style={styles.picker}
-      >
-        {/* Popola il Picker con gli username degli utenti */}
-        {users.map((user: User) => (
-          <Picker.Item
-            key={user.username}
-            label={user.username}
-            value={user.username}
-          />
-        ))}
-      </Picker>
-    </View>
-  );
-};
-
-// Tipi delle props per il componente
-interface CouponCardProps {
-  restaurant: {
-    name: string;
-    logo?: string; // URL del logo
-  };
-  discount: string; // Testo dello sconto
-  expiryDate: string; // Data di scadenza del coupon
-  qrCodeLink: string; // Valore del QR Code (es. URL o stringa univoca)
-  onPressQRCode: (code: string) => void;
-}
-
-const CouponCard: React.FC<CouponCardProps> = ({ restaurant, discount, expiryDate, qrCodeLink, onPressQRCode }) => {
-  return (
-    <View style={styles.qrCardContainer}>
-      {/* Titolo del Coupon */}
-      <Text style={styles.qrCardTitle}>{restaurant.name}</Text>
-
-      {/* Dettagli del Coupon */}
-      <Text style={styles.qrCardDiscount}>{discount}</Text>
-      <Text style={styles.qrCardExpiry}>Valid until: {expiryDate}</Text>
-
-      {/* QR Code */}
-      <TouchableOpacity
-        style={styles.qrDataCard}
-        onPress={() => onPressQRCode(qrCodeLink)}
-      >
-      <QRCode value={qrCodeLink} size={150} />
-      </TouchableOpacity>
-
-      {/* Istruzioni */}
-      <Text style={styles.qrDataText}>
-      Scan the QR code to redeem your discount!
-      </Text>
-    </View>
-  );
-};
-
 interface ProfileScreenProps {
   user: User | undefined;
   users: User[];
   setUser: React.Dispatch<React.SetStateAction<User | undefined>>;
-  qrCodeLink: string; 
+  qrCodeLink: string;
 }
 
 interface QRCodeData {
   restaurantName: string;
   discount: string;
   expiryDate: string;
- 
 }
 
 const qrCode: QRCodeData = {
@@ -130,7 +65,6 @@ const qrCode: QRCodeData = {
   expiryDate: "2025-01-31",
 };
 
-// Mapping delle immagini
 const userImages: { [key: string]: any } = {
   giacomo_gugu: require("../../assets/img/profile/giacomo_gugu.png"),
   alice_gugu: require("../../assets/img/profile/alice_gugu.png"),
@@ -143,21 +77,25 @@ const ProfileScreen: FC<ProfileScreenProps> = ({ user, users, setUser, qrCodeLin
     user?.username
   );
 
-  // Stato per il QR Code selezionato e la visibilità del Modal
-  const [selectedQR, setSelectedQR] = useState<string | null>(null);
-  const [modalVisible, setModalVisible] = useState(false);
-  const [modalUserChoice, setModalUserChoice] = useState(false);
 
-  // Funzione per aprire il Modal
-  const openModal = (code: string) => {
-    setSelectedQR(code);
-    setModalVisible(true);
-  };
-
-  // Funzione per chiudere il Modal
-  const closeModal = () => {
-    setSelectedQR(null);
-    setModalVisible(false);
+  const showActionSheet = () => {
+    const options = users.map((user) => user.username).concat("Cancel");
+    ActionSheetIOS.showActionSheetWithOptions(
+      {
+        options: options,
+        cancelButtonIndex: options.length - 1,
+      },
+      (buttonIndex) => {
+        if (buttonIndex !== options.length - 1) {
+          const selectedUsername = options[buttonIndex];
+          const selected = users.find((u) => u.username === selectedUsername);
+          if (selected) {
+            setUser(selected);
+            setSelectedUser(selected.username);
+          }
+        }
+      }
+    );
   };
 
   useEffect(() => {
@@ -173,15 +111,6 @@ const ProfileScreen: FC<ProfileScreenProps> = ({ user, users, setUser, qrCodeLin
     }
   }, [selectedUser, users, setUser]);
 
-  const restaurant = {
-    name: "Ristorante Bella Italia",
-   // logo: "../../icon.png", // Sostituisci con un URL reale
-  };
-
-  const discount = "20% discount on your current meal";
-  const expiryDate = "31/01/2025";
-  const qrValue = "https://ristorantebellaitalia.com/riscatta-sconto";
-
   return (
     <KeyboardAwareScrollView
       contentContainerStyle={styles.scrollViewContent}
@@ -196,7 +125,7 @@ const ProfileScreen: FC<ProfileScreenProps> = ({ user, users, setUser, qrCodeLin
       >
         <View style={styles.profileHeaderContainer}>
           <View style={styles.nameRoleContainer}>
-            <TouchableOpacity onPress={() => setModalUserChoice(true)}>
+            <TouchableOpacity onPress={showActionSheet}>
               <Text style={styles.name}>
                 {user?.username || "Username non disponibile"}
               </Text>
@@ -222,7 +151,7 @@ const ProfileScreen: FC<ProfileScreenProps> = ({ user, users, setUser, qrCodeLin
           </View>
         </View>
         {/* Input fields container */}
-        <View>
+        <View >
           {/* All the input fields */}
           <CustomInput
             label="First name:"
@@ -239,79 +168,13 @@ const ProfileScreen: FC<ProfileScreenProps> = ({ user, users, setUser, qrCodeLin
             icon={<Ionicons name="mail-outline" size={iconSize.medium} />}
             value={user?.email}
           />
-         
           <CustomInput
             placeholder="+39 1234567890"
             icon={<Feather name="phone" size={iconSize.medium} />}
             value={user?.phone_number}
           />
         </View>
-
-      {/* Coupon Card */}
-      
-     {/*<CouponCard
-        restaurant={restaurant}
-        discount={discount}
-        expiryDate={expiryDate}
-        qrCodeLink={qrCodeLink}
-        onPressQRCode={openModal}
-      /> */}
-     
-    </View>
-      {/* Modal per mostrare il QR Code ingrandito */}
-      <Modal
-        visible={modalVisible}
-        transparent={true} // Sfondo trasparente
-        animationType="fade" // Animazione
-        onRequestClose={closeModal} // Chiude il Modal
-      >
-        <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
-            {/* QR Code ingrandito */}
-            {selectedQR && <QRCode value={selectedQR} size={300} />}
-            <Text style={styles.modalText}>Scan the QR Code</Text>
-            {/* Bottone per chiudere il Modal */}
-            <TouchableOpacity style={styles.closeButton} onPress={closeModal}>
-              <Text style={styles.closeButtonText}>Close</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </Modal>
-
-      <Modal
-        visible={modalUserChoice}
-        transparent={true} // Sfondo trasparente
-        animationType="fade" // Animazione
-        onRequestClose={() => setModalUserChoice(false)} // Chiude il Modal
-      >
-        <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
-            {/* Dropdown per selezionare l'utente */}
-            <Dropdown
-              users={users} // Passiamo gli utenti al Dropdown
-              selectedValue={selectedUser} // Valore selezionato nel dropdown
-              onValueChange={setSelectedUser} // Funzione per aggiornare la selezione
-            />
-            <View style={styles.modalButtonsContainer}>
-              {/* Bottone per confermare la selezione */}
-              <TouchableOpacity
-                style={styles.confirmButton}
-                onPress={() => {
-                  const selected = users.find(
-                    (u) => u.username === selectedUser
-                  );
-                  if (selected) {
-                    setUser(selected);
-                  }
-                  setModalUserChoice(false); // Chiudi il Modal
-                }}
-              >
-                <Text style={styles.confirmButtonText}>Close</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </View>
-      </Modal>
+      </View>
     </KeyboardAwareScrollView>
   );
 };
